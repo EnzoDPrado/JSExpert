ffa3a5565125d5b2454e218a8c13ed6fc531007c
EnzoDPrado <enzodp1501@gmail.com>
2024-10-14 10:32:29 -0300

    start template method

---
+++ module08/behavioral/03.template-method/src/business/base/baseBusiness.js
@@ -0,0 +1,16 @@
+import { NotImplementedException } from "../../util/exceptions";
+
+export default class BaseBusiness {
+  _validateRequiredFields(data){
+    
+  }
+
+  _create(data){
+
+  }
+
+  create(data){
+    // validar campos
+    //  salvar no banco
+  }
+}
