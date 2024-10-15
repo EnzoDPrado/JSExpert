84b03f9eb3e8b0a24d13c33533a56f3c12a34dfa
EnzoDPrado <enzodp1501@gmail.com>
2024-10-15 18:09:26 -0300

    decorator class

---
+++ module08/structural/04.decorator-middleware/example/http-server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+import { InjectHttpInterceptor } from '../src/agent.js';
+
+InjectHttpInterceptor();
+
+function handleRequest(request, response) {
+  response.end('Hello world!');
+}
+
+const server = http.createServer(handleRequest);
+const port = 3000;
+server.listen(port, () =>
+  console.log('Server running at', server.address().port)
+);
---
+++ module08/structural/04.decorator-middleware/src/agent.js
@@ -0,0 +1,17 @@
+import Http from 'http';
+
+export async function InjectHttpInterceptor() {
+  const oldEmit = Http.Server.prototype.emit;
+
+  Http.Server.prototype.emit = function(...args) {
+    const [type, req, response] = args
+
+    if(type === 'request'){
+      response.setHeader('X-Instrumented-By', 'EnzoDPrado')
+    }
+
+    return oldEmit.apply(this, args)
+  }
+
+}
+
